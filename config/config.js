0ff543f0c145498dc232ac44309a6ec7e7abf662
Sad4k <thsmall99@gmail.com>
2024-05-29 17:14:08 -0400

    db change

--- config/config.js
+++ config/config.js
@@ -1,4 +1,16 @@
 // config.js
+const serverConfig = {
+getConfigServer: () => {
+  if (!fs.existsSync(configFilePath)) {
+    throw new Error('El archivo de configuración no existe.');
+  }
+
+  const rawData = fs.readFileSync(configFilePath, 'utf8');
+  const configs = JSON.parse(rawData);
+
+  return configs;
+}
+}
 module.exports = {
   //ssh config
   ssh: {
@@ -7,4 +19,5 @@ module.exports = {
     username: 'sad4k',
     password: '12345s3561a',
   },
+  serverConfig
 };
